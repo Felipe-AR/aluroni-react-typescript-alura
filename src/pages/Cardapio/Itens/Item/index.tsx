878e76fce4aff7158e0c3c5fb3921df95071d3d0
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-29 20:18:16 -0300

    Navegação para os detalhes e estilização do Item

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
 import TagsPrato from 'components/TagsPrato';
+import { useNavigate } from 'react-router-dom';
 
 export default function Item(props: Prato) {
-  const { title, description, photo } = props;
+  const navigate = useNavigate();
+  const { id, title, description, photo } = props;
   return (
-    <div className={styles.item}>
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
       <div className={styles.item__imagem}>
-        <img src={photo} alt="" />
+        <img src={photo} alt={title} />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>

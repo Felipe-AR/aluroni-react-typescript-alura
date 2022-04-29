fd22b1802025e1a6746978a1d61c316f411793ab
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-29 13:18:23 -0300

    Utilização de rotas

---
+++ src/routes.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Menu from 'components/Menu';
+import Cardapio from 'pages/Cardapio';
+import Inicio from 'pages/Inicio';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PaginaPadrao from 'components/PaginaPadrao';
+import Sobre from 'pages/Sobre';
+import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
+
+export default function AppRouter() {
+  return (
+    <main className="container">
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+            <Route path="sobre" element={<Sobre />} />
+            <Route path="prato/:id" element={<Prato />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
+  );
+}

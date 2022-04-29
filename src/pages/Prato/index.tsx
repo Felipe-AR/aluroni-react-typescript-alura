b1b0b8a6bb212e4c2e266bb425a785f39c823ee1
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-29 20:17:22 -0300

    Subrotas sobre a página de Prato

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import styles from './Prato.module.scss';
 import cardapio from 'data/cardapio.json';
+import NotFound from 'pages/NotFound';
+import PaginaPadrao from 'components/PaginaPadrao';
 import TagsPrato from 'components/TagsPrato';
 
 export default function Prato() {
@@ -10,24 +12,35 @@ export default function Prato() {
   const prato = cardapio.find(item => item.id === Number(id));
 
   if (!prato) {
-    return <div></div>;
+    return <NotFound />;
   }
 
   return (
-    <React.Fragment>
-      <button className={styles.voltar} onClick={() => navigate(-1)}>
-        {'< Voltar'}
-      </button>
-      <section className={styles.container}>
-        <h1 className={styles.titulo}>{prato.title}</h1>
-        <div className={styles.imagem}>
-          <img src={prato.photo} alt={prato.title} />
-        </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>{prato.description}</p>
-          <TagsPrato {...prato} />
-        </div>
-      </section>
-    </React.Fragment>
+    <Routes>
+      <Route path="*" element={<PaginaPadrao />}>
+        <Route
+          index
+          element={
+            <>
+              <button className={styles.voltar} onClick={() => navigate(-1)}>
+                {'< Voltar'}
+              </button>
+              <section className={styles.container}>
+                <h1 className={styles.titulo}>{prato.title}</h1>
+                <div className={styles.imagem}>
+                  <img src={prato.photo} alt={prato.title} />
+                </div>
+                <div className={styles.conteudo}>
+                  <p className={styles.conteudo__descricao}>
+                    {prato.description}
+                  </p>
+                  <TagsPrato {...prato} />
+                </div>
+              </section>
+            </>
+          }
+        ></Route>
+      </Route>
+    </Routes>
   );
 }

21a7465183d56f60b290c729d2448b0770d110b1
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-29 20:17:58 -0300

    Possibilidade de repassar dados como children da página padrão

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -3,7 +3,11 @@ import { Outlet } from 'react-router-dom';
 import styles from './PaginaPadrao.module.scss';
 import stylesTema from 'styles/Tema.module.scss';
 
-export default function PaginaPadrao() {
+export default function PaginaPadrao({
+  children
+}: {
+  children?: React.ReactNode;
+}) {
   return (
     <React.Fragment>
       <header className={styles.header}>
@@ -11,6 +15,7 @@ export default function PaginaPadrao() {
       </header>
       <div className={stylesTema.container}>
         <Outlet />
+        {children}
       </div>
     </React.Fragment>
   );

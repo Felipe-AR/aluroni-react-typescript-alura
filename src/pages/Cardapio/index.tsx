6368db3e4531425f99730f278ae8d1c4918f0cb5
Felipe-AR <fefeucn_cm@hotmail.com>
2022-04-29 13:24:21 -0300

    Remoção  do Header da página de cardápio

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import styles from './Cardapio.module.scss';
-import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from './Buscador';
 import Filtros from './Filtros';
 import Ordenador, { OpcoesOrdenador } from './Ordenador';
 import Itens from './Itens';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
@@ -12,16 +12,9 @@ export default function Cardapio() {
   const [ordenador, setOrdenador] = useState<OpcoesOrdenador>('');
 
   return (
-    <main>
-      <nav className={styles.menu}>
-        <Logo title="Logo do Aluroni"></Logo>
-        Aluroni
-      </nav>
-      <header className={styles.header}>
-        <div className={styles.header__text}>Casa do Codigo e da Massa</div>
-      </header>
+    <React.Fragment>
       <section className={styles.cardapio}>
-        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <h3 className={stylesTema.titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro} />
@@ -29,6 +22,6 @@ export default function Cardapio() {
         </div>
         <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
       </section>
-    </main>
+    </React.Fragment>
   );
 }
